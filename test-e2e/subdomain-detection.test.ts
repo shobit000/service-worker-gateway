46cbc31c0ccb957025a42e40ae23387d503325a4
Shobit garg <122355051+shobit000@users.noreply.github.com>
2025-03-16 23:02:23 +0530

    Update subdomain-detection.test.ts

--- test-e2e/subdomain-detection.test.ts
+++ test-e2e/subdomain-detection.test.ts
@@ -12,31 +12,30 @@ test.describe('subdomain-detection', () => {
     gateways.push(process.env.KUBO_GATEWAY)
     routers.push(process.env.KUBO_GATEWAY)
   })
-  test('path requests are redirected to subdomains', async ({ page, baseURL, rootDomain, protocol }) => {
-    await page.goto(baseURL, { waitUntil: 'networkidle' })
-    await waitForServiceWorker(page)
-    await setConfig({
-      page,
-      config: {
-        gateways,
-        routers,
-        dnsJsonResolvers: {
-          '.': 'https://delegated-ipfs.dev/dns-query'
-        }
+  test('path requests with query params are redirected to subdomains', async ({ page, baseURL, rootDomain, protocol }) => {
+  await page.goto(baseURL, { waitUntil: 'networkidle' })
+  await waitForServiceWorker(page)
+  await setConfig({
+    page,
+    config: {
+      gateways,
+      routers,
+      dnsJsonResolvers: {
+        '.': 'https://delegated-ipfs.dev/dns-query'
       }
-    })
-    const initialResponse = await page.goto('/ipfs/bafkqablimvwgy3y', { waitUntil: 'commit' })
-
-    expect(initialResponse?.url()).toBe(`${protocol}//bafkqablimvwgy3y.ipfs.${rootDomain}/`)
-    expect(initialResponse?.request()?.redirectedFrom()?.url()).toBe(`${protocol}//${rootDomain}/ipfs/bafkqablimvwgy3y`)
-
-    await page.waitForURL(`${protocol}//bafkqablimvwgy3y.ipfs.${rootDomain}`)
-    const bodyTextLocator = page.locator('body')
-
-    await waitForServiceWorker(page)
-
-    await expect(bodyTextLocator).toContainText('hello')
+    }
   })
+  const initialResponse = await page.goto('/ipfs/bafkqablimvwgy3y?format=car', { waitUntil: 'commit' })
+
+  expect(initialResponse?.url()).toBe(`${protocol}//bafkqablimvwgy3y.ipfs.${rootDomain}/?format=car`)
+  expect(initialResponse?.request()?.redirectedFrom()?.url()).toBe(`${protocol}//${rootDomain}/ipfs/bafkqablimvwgy3y?format=car`)
+
+  await page.waitForURL(`${protocol}//bafkqablimvwgy3y.ipfs.${rootDomain}?format=car`)
+  const bodyTextLocator = page.locator('body')
+
+  await waitForServiceWorker(page)
+
+  await expect(bodyTextLocator).toContainText('hello')
 })
 
 testSubdomainRouting.describe('subdomain-detection auto fixture', () => {
